713859e8a946ba77eada6f288c7d4c62eac733fc
Tim <15017472+doodlum@users.noreply.github.com>
2023-07-14 17:48:35 +0100

    Block Cancel addon

--- src/MCO.h
+++ src/MCO.h
@@ -14,57 +14,24 @@ public:
 		Hooks::Install();
 	}
 
-	enum class Direction {
-		kNeutral = 0,
-		kForward,
-		kStrafeRight,
-		kBack,
-		kStrafeLeft,
-	};
-
-	enum class DirectionOcto {
-		kNeutral = 0,
-		kForward,
-		kForwardRight,
-		kStrafeRight,
-		kBackRight,
-		kBack,
-		kBackLeft,
-		kStrafeLeft,
-		kForwardLeft
-	};
-
-	Direction GetDirection(RE::NiPoint2 a_vec, bool a_gamepad);
-	DirectionOcto GetDirectionOcto(RE::NiPoint2 a_vec, bool a_gamepad);
-
-	void ProcessMovement(RE::PlayerControlsData* a_data, bool a_gamepad);
+	void ProcessBlock();
 
 	struct Hooks
 	{
-		struct MovementHandler_ProcessThumbstick
-		{
-			static void thunk(RE::MovementHandler* a_this, RE::ThumbstickEvent* a_event, RE::PlayerControlsData* a_data)
-			{
-				func(a_this, a_event, a_data);
-				GetSingleton()->ProcessMovement(a_data, true);
-			}
-			static inline REL::Relocation<decltype(thunk)> func;
-		};
 
-		struct MovementHandler_ProcessButton
+		struct AttackBlockHandler_ProcessButton
 		{
-			static void thunk(RE::MovementHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
+			static void thunk(RE::AttackBlockHandler* a_this, RE::ButtonEvent* a_event, RE::PlayerControlsData* a_data)
 			{
 				func(a_this, a_event, a_data);
-				GetSingleton()->ProcessMovement(a_data, false);
+				GetSingleton()->ProcessBlock();
 			}
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
 
 		static void Install()
 		{
-			stl::write_vfunc<0x2, MovementHandler_ProcessThumbstick>(RE::VTABLE_MovementHandler[0]);
-			stl::write_vfunc<0x4, MovementHandler_ProcessButton>(RE::VTABLE_MovementHandler[0]);
+			stl::write_vfunc<0x4, AttackBlockHandler_ProcessButton>(RE::VTABLE_AttackBlockHandler[0]);
 		}
 	};
 private:
